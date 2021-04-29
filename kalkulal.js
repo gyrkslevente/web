d103f840298ef11e3819a5f2de3bb2bbcd744403
Lopatovszki Titán <lopatovszki.titan@gmail.com>
2021-04-29 11:58:42 +0200

    first

---
+++ kalkulal.js
@@ -0,0 +1,17 @@
+function szamol() {
+    var letszam = document.getElementById("letszam").value;
+    var ejszaka = document.getElementById("ejszaka").value;
+    var orszag = document.getElementById("orszag").value;
+    var fizetendo;
+    var ar = {};
+
+    ar["b"] = 5600;
+    ar["g"] = 6200;
+    ar["h"] = 5000;
+    ar["t"] = 5800;
+    //--számolás--
+    fizetendo = parseInt(letszam) * parseInt(ejszaka) * ar[orszag];
+    //------------
+    document.getElementById('eredmeny').value = fizetendo.toLocaleString() +" Ft";
+    
+}
